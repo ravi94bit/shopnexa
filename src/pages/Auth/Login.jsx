031517c103f0eb1b5129f7caf89911b4e2b67c29
RaviSingh <Ravi.singh@richestsoft.in>
2025-07-22 16:11:28 +0530

    fix3

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -60,7 +60,7 @@ const Login = () => {
         </form>
 
         <p className="switch-text">
-          Don’t have an account? <Link to="/register">Register here add 1 </Link>
+          Don’t have an account? <Link to="/register">Register here </Link>
         </p>
         
       </div>
